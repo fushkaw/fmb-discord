b9534ab6da7a06d5d489818cf29eae137543da18
fushka <125378211+fushkaw@users.noreply.github.com>
2023-03-24 12:25:37 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 settings = {
-    'token': 'MTA2Mzg5MTU4ODkxODU2Mjk1Ng.GbSAQ1.sTeUWBZ32D8OonIW5I-YfD2wL9nICtiNhzGLQE',
+    'token': 'token',
     'id': 1063891588918562956,
     'prefix': 'fbm.'
 }
